9e7bf7376b787cfdcf4602cef821971556150c9f
zapretsuperapp <poshulnaher@vk.com>
2025-02-28 17:17:11 +0300

    Create urls.py

---
+++ urls.py
@@ -0,0 +1,9 @@
+# urls.py
+
+# URLs for updates and documentation
+VERSION_URL = "НАПИШИТЕДЛЯПОЛУЧЕНИЯСССЫЛКИ"  # URL для проверки обновлений
+UPDATER_BAT_URL = "НАПИШИТЕДЛЯПОЛУЧЕНИЯСССЫЛКИ"  # URL для скачивания BAT-файла обновления (убрал лишнюю букву h)
+EXE_UPDATE_URL = "НАПИШИТЕДЛЯПОЛУЧЕНИЯСССЫЛКИ"  # URL для скачивания обновления EXE
+INFO_URL = "https://zapret.vercel.app/info.html"  # URL с информацией о программе
+AUTHOR_URL = "https://t.me/bypassblock"  # URL автора
+PROXY_DOMAINS_URL = "https://zapret.vercel.app/proxy_domains.txt"  # URL со списком прокси-доменов
