52fd86b07734cb03a7dfff7a8f18c430ed5614e6
zapretsuperapp <poshulnaher@vk.com>
2025-05-17 20:46:28 +0300

    15.7

--- downloader.py
+++ downloader.py
@@ -6,16 +6,16 @@ DOWNLOAD_URLS = {
     "winws.exe": "https://github.com/bol-van/zapret-win-bundle/raw/refs/heads/master/zapret-winws/winws.exe",
     "WinDivert.dll": "https://github.com/bol-van/zapret-win-bundle/raw/refs/heads/master/zapret-winws/WinDivert.dll",
     "WinDivert64.sys": "https://github.com/bol-van/zapret-win-bundle/raw/refs/heads/master/zapret-winws/WinDivert64.sys",
-    "cygwin1.dll": "https://github.com/bol-van/zapret-win-bundle/raw/refs/heads/master/zapret-winws/cygwin1.dll"
+    "cygwin1.dll": "https://github.com/bol-van/zapret-win-bundle/raw/refs/heads/master/zapret-winws/cygwin1.dll",
+    "stop.bat": "https://gitflic.ru/project/main1234/main1234/blob/raw?file=stop.bat",
 }
 
-def download_files(bin_folder, lists_folder, download_urls, status_callback=None):
+def download_files(bin_folder, download_urls, status_callback=None):
     """
     Скачивает необходимые файлы с GitHub
     
     Args:
         bin_folder (str): Путь к папке bin
-        lists_folder (str): Путь к папке lists
         download_urls (dict): Словарь с именами файлов и URL для скачивания
         status_callback (function): Функция для отображения статуса скачивания
         
@@ -25,14 +25,14 @@ def download_files(bin_folder, lists_folder, download_urls, status_callback=None
     try:
         # Создаем папки если они не существуют
         os.makedirs(bin_folder, exist_ok=True)
-        os.makedirs(lists_folder, exist_ok=True)
         
+        from log import log
         # Функция для вывода статуса, если передана
         def set_status(message):
             if status_callback:
                 status_callback(message)
             else:
-                print(message)
+                log(message, level="DOWNLOAD")
         
         # Проверяем, существуют ли все файлы
         all_files_exist = True
@@ -44,7 +44,7 @@ def download_files(bin_folder, lists_folder, download_urls, status_callback=None
                 
         # Если все файлы уже существуют, сообщаем об этом
         if all_files_exist:
-            print("Все файлы уже загружены и готовы к использованию")
+            log(f"Все файлы уже загружены и готовы к использованию", level="DOWNLOAD")
             set_status("Все файлы готовы к использованию")
             return True
         
@@ -54,18 +54,19 @@ def download_files(bin_folder, lists_folder, download_urls, status_callback=None
             
             # Проверяем, существует ли файл уже
             if os.path.exists(filepath):
-                set_status(f"Файл {filename} уже существует")
+                log(f"Файл {filename} уже существует", level="DOWNLOAD")
                 continue
-                
-            set_status(f"Скачиваем {filename}...")
+
+            log(f"Файл {filename} не найден, скачиваем...", level="DOWNLOAD")
             response = requests.get(url, stream=True)
             
             if response.status_code == 200:
                 with open(filepath, 'wb') as f:
                     response.raw.decode_content = True
                     shutil.copyfileobj(response.raw, f)
-                set_status(f"Файл {filename} скачан успешно")
+                log(f"Файл {filename} скачан успешно", level="DOWNLOAD")
             else:
+                log(f"Ошибка при скачивании {filename}, код: {response.status_code}", level="ERROR")
                 raise Exception(f"Не удалось скачать {filename}, код: {response.status_code}")
                 
         # Создаем пустые txt файлы в lists, если их нет
@@ -75,26 +76,26 @@ def download_files(bin_folder, lists_folder, download_urls, status_callback=None
             "youtube.txt",
             "youtube_v2.txt",
             "youtubeGV.txt", 
-            "other.txt", 
             "discord.txt",
             "faceinsta.txt",
             "russia-youtube-rtmps.txt",
-            "ipset-discord.txt"
+            "ipset-discord.txt",
+            "ipset-cloudflare.txt"
         ]
         
         for listfile in default_lists:
-            filepath = os.path.join(lists_folder, listfile)
+            filepath = os.path.join(bin_folder, listfile)
             if not os.path.exists(filepath):
                 with open(filepath, 'w', encoding='utf-8') as f:
                     f.write("# Добавьте адреса сайтов по одному на строку\n")
-                    
-        print("Все файлы успешно загружены и готовы к использованию")
-        set_status("Все файлы готовы")
+
+        log("Все файлы успешно загружены и готовы к использованию", level="DOWNLOAD")
+        log(f"======================== ВСЕ ФАЙЛЫ ГОТОВЫ ========================", level="DOWNLOAD")
         return True
                 
     except Exception as e:
         error_msg = f"Ошибка при скачивании файлов: {str(e)}"
-        print(error_msg)
+        log(f"Ошибка при скачивании файлов: {error_msg}", level="ERROR")
         if status_callback:
             status_callback(error_msg)
         return False
